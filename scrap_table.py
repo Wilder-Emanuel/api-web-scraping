8cc60e890cc8752c9a147eda401dcc3e072a64f2
Wilder-Emanuel <165910409+Wilder-Emanuel@users.noreply.github.com>
2024-11-06 10:01:09 -0500

    Update scrap_table.py

--- scrap_table.py
+++ scrap_table.py
@@ -4,8 +4,8 @@ import boto3
 import uuid
 
 def lambda_handler(event, context):
-    # URL de la página web que contiene la tabla
-    url = "https://ultimosismo.igp.gob.pe/ultimosismo/sismos-reportados"
+    # URL de la página web que contiene la tabla de sismos
+    url = "https://ultimosismo.igp.gob.pe/ultimo-sismo/sismos-reportados"
 
     # Realizar la solicitud HTTP a la página web
     response = requests.get(url)
@@ -26,36 +26,36 @@ def lambda_handler(event, context):
             'body': 'No se encontró la tabla en la página web'
         }
 
-    # Extraer los encabezados de la tabla
-    headers = [header.text for header in table.find_all('th')]
+    # Definir los encabezados de la tabla para los datos relevantes
+    headers = ['Reporte Sísmico', 'Referencia', 'Fecha y Hora', 'Magnitud']
 
     # Extraer las filas de la tabla
     rows = []
     for row in table.find_all('tr')[1:]:  # Omitir el encabezado
         cells = row.find_all('td')
-        rows.append({headers[i+1]: cell.text for i, cell in enumerate(cells)})
+        if len(cells) >= 4:  # Asegurar que hay suficientes columnas
+            data = {
+                'ReporteSismico': cells[0].text.strip(),
+                'Referencia': cells[1].text.strip(),
+                'FechaHora': cells[2].text.strip(),
+                'Magnitud': cells[3].text.strip(),
+                'id': str(uuid.uuid4())  # Generar un ID único para cada entrada
+            }
+            rows.append(data)
 
     # Guardar los datos en DynamoDB
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('TablaWebScrappingSismosDev')
+    table = dynamodb.Table('TablaWebScrapping')
 
     # Eliminar todos los elementos de la tabla antes de agregar los nuevos
     scan = table.scan()
     with table.batch_writer() as batch:
         for each in scan['Items']:
-            batch.delete_item(
-                Key={
-                    'id': each['id']
-                }
-            )
+            batch.delete_item(Key={'id': each['id']})
 
     # Insertar los nuevos datos
-    i = 1
     for row in rows:
-        row['#'] = i
-        row['id'] = str(uuid.uuid4())  # Generar un ID único para cada entrada
         table.put_item(Item=row)
-        i += 1
 
     # Retornar el resultado como JSON
     return {
