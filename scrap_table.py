e3e40caf05718adf427d3c6efa1a35c8a3946a1f
Wilder-Emanuel <165910409+Wilder-Emanuel@users.noreply.github.com>
2024-11-06 11:13:53 -0500

    Update scrap_table.py

--- scrap_table.py
+++ scrap_table.py
@@ -5,10 +5,10 @@ from bs4 import BeautifulSoup
 
 # Configuración de DynamoDB
 dynamodb = boto3.resource('dynamodb')
-table_name = os.environ.get('DYNAMODB_TABLE', 'TablaWebScrappingSismosDev')
+table_name = os.environ.get('DYNAMODB_TABLE', 'TablaWebScrapping')
 table = dynamodb.Table(table_name)
 
-def scrape_and_store():
+def lambda_handler(event, context):
     # Leer el contenido del archivo HTML
     with open("htmlSismos.txt", "r", encoding="utf-8") as file:
         html_content = file.read()
@@ -28,13 +28,13 @@ def scrape_and_store():
     rows = []
     for row in table.find('tbody').find_all('tr'):
         cells = row.find_all('td')
-        if len(cells) >= 4:  # Asegurarse de que hay suficientes columnas
+        if len(cells) >= 4:
             data = {
                 'ReporteSismico': cells[0].text.strip(),
                 'Referencia': cells[1].text.strip(),
                 'FechaHora': cells[2].text.strip(),
                 'Magnitud': cells[3].text.strip(),
-                'id': str(uuid.uuid4())  # Generar un ID único para cada entrada
+                'id': str(uuid.uuid4())
             }
             rows.append(data)
 
@@ -47,7 +47,3 @@ def scrape_and_store():
         'statusCode': 200,
         'body': 'Datos de sismos guardados correctamente en DynamoDB'
     }
-
-# Llamar a la función principal si es necesario
-if __name__ == "__main__":
-    scrape_and_store()
